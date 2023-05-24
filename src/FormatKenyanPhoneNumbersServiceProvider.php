9fa05dccecae9852b09278bf5ee698071fae284e
bnjunge <survtechke@gmail.com>
2023-05-24 13:47:43 +0300

    Update readme

---
+++ src/FormatKenyanPhoneNumbersServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BNjunge;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormatKenyanPhoneNumbersServiceProvider extends ServiceProvider {
+
+    public function boot(){
+        // echo 'It Works';
+    }
+
+    public function register(){
+        
+    }
+}
